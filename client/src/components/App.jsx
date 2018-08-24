36439943207e8da4ff83077f5e8226eaa78227fa
key-er <nasaquba@gmail.com>
2018-08-24 03:05:30 -0700

    modified UI and added dummy table

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -5,26 +5,34 @@ import List from './ListServices.jsx';
 import { Grid, Image, Button, Dropdown } from 'semantic-ui-react';
 import _ from 'lodash'
 import { } from 'semantic-ui-react'
-
+import TableSelectableRow from './TableSelectableRow.jsx'
 
 const caseSensitiveSearch = (options, query) => {
   const re = new RegExp(_.escapeRegExp(query))
 }
 
-const options = [
-  { key: 'a', value: 'a', text: 'UPPERCASE' },
-  { key: 'b', value: 'b', text: 'lowercase' },
+const case_options = [
+  { key: 'a', value: 'a', text: 'all' },
+  { key: 'b', value: 'b', text: 'case-abc' },
+  { key: 'c', value: 'c', text: 'case-mini' },
 ]
 
-const DropdownExampleCustomSearchFunction = () => (
+const type_options = [
+  { key: 'a', value: 'a', text: 'all' },
+  { key: 'b', value: 'b', text: 'ip' },
+  { key: 'c', value: 'c', text: 'domain' },
+  { key: 'c', value: 'c', text: 'os-version' },
+]
+const DropdownExampleCustomSearchFunction = (props) => (
   <Dropdown
-    fluid
-    options={options}
+
+    options={props.options}
     placeholder={'Try to search for case or CASE'}
     search={caseSensitiveSearch}
     selection
   />
 )
+
 
 
 
@@ -36,18 +44,20 @@ const BrownGrid = () => (
         <Image circular src='brown3_lrge.png' />
       </Grid.Column>
       <Grid.Column width={14}>
-        <input type="text" />
-        <DropdownExampleCustomSearchFunction/>
+        <input type="text" id="fname" name="fname" />
+        <DropdownExampleCustomSearchFunction options={case_options}/>
+         <Button inverted color='brown' content='Search...' />
+
+        <DropdownExampleCustomSearchFunction options={type_options}/>
          <Button inverted color='brown' content='Search...' />
       </Grid.Column>
+
     </Grid.Row>
 
     <Grid.Row>
-      <Grid.Column width={6}>
-        10 Recently Added IOCs
-      </Grid.Column>
       <Grid.Column width={10}>
         table would appear here
+        <TableSelectableRow />
       </Grid.Column>
     </Grid.Row>
   </Grid>
@@ -81,8 +91,6 @@ class App extends React.Component {
   render() {
     return (<div>
       <BrownGrid />
-      <h1>Item List</h1>
-      <List services={this.state.services}/>
     </div>)
   }
 }
