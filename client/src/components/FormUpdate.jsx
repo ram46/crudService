acd73aff4e1eb971133337be849f1afbe30edac1
key-er <nasaquba@gmail.com>
2018-08-25 06:46:21 -0700

    implemented modify of crud

--- client/src/components/FormUpdate.jsx
+++ client/src/components/FormUpdate.jsx
@@ -1,15 +1,76 @@
 import React from 'react';
 import { Grid, Image, Button, Dropdown, Form } from 'semantic-ui-react';
+import $ from 'jquery'
+
+class FormUpdate extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.handleChange  = this.handleChange.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+    this.handleKeyPress = this.handleKeyPress.bind(this)
+
+    this.state = {
+      afterUpdateView: false,
+      inputData: null,
+      outputData:null
+    }
+  }
 
 
-const FormUpdate = () => (
-  <Form unstackable>
-    <Form.Group widths={2}>
-      <Form.Input control='textarea' rows='10' label='Update IOC' placeholder='[{"id":"71281621", "notes":"updated notes"}, {"id":"3321", "case": "brownie"}]' />
-    </Form.Group>
-    <Button type='submit'>Submit</Button>
-  </Form>
-)
+  handleChange(event) {
+    this.setState({
+      inputData: event.target.value
+    })
+  }
+
+  handleClick(event) {
+    debugger
+
+    var self = this;
+    $.ajax({
+      url: 'updateioc',
+      method: 'POST',
+      data: {query: this.state.inputData},
+      context: self,
+      success: function(data) {
+        this.setState({
+          outputData: data,
+          afterUpdateView: true,
+         })
+      },
+      error: function(err) {
+
+      }
+    })
+
+
+
+  }
+
+  handleKeyPress(event) {
+
+  }
+
+  render() {
+    var content;
+    var button;
+
+    if (!this.state.afterUpdateView) {
+      button = <Button type='submit'onClick={this.handleClick} >Submit</Button>
+      content = <Form unstackable> <Form.Group widths={2}> <Form.Input onChange={this.handleChange}  control='textarea' rows='10' label='Update IOC' placeholder='{"newValue": {"ioc": "2.2.2.2"}, "where":  {"where": {"ioc" : "5.5.5.5"} }}' /> </Form.Group> {button} </Form>
+    }
+
+
+    if (this.state.afterUpdateView) {
+      content = <textarea rows="4" cols="50"> {this.state.outputData}</textarea>
+    }
+
+    return content
+      }
+  }
+
 
 
 
--- database-mysql/model.js
+++ database-mysql/model.js
@@ -2,7 +2,8 @@ const IOC = require('./index.js').IOC
 
 module.exports = {
   create: function(iocs) {
-
+    // iocs is an array
+    IOC.bulkCreate(iocs)
   },
 
   read: function(filter, cb) {
@@ -19,7 +20,20 @@ module.exports = {
   },
 
 
-  update: function(iocs) {
+  update: function(query, cb) {
+    // var query = {"newValue": {"ioc": "3.3.2.2"}, "where":  {"where": {"ioc" : "2.2.2.2"} }}
+
+    IOC.update(query.newValue, query.where).then((result) => {
+      // cb(null, result)
+    }).spread((affectedCount, affectedRows) => {
+        return IOC.findAll();
+    }).then(iocs => {
+      cb(null, iocs)
+      console.log(iocs, affectedCount)
+    }).catch((err) => {
+      cb(err, null);
+    })
+
 
   },
 
@@ -27,3 +41,10 @@ module.exports = {
 
   }
 }
+
+
+// var query = {"newValue": {"ioc": "3.2.3.2"}, "where":  {"where": {"ioc" : "4.4.4.4"} }}
+// module.exports.update(query, () => {})
+
+
+
