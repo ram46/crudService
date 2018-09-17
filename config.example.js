63d2af9dfb2603733fa75b11bb5c0bd51e0f1cd3
key-er <nasaquba@gmail.com>
2018-09-16 23:29:29 -0700

    config so it works on localhost

--- config.example.js
+++ config.example.js
@@ -1,6 +1,6 @@
 module.exports = {
-  HOST     : 'cruddb-node',
-  USER    : process.env.MYSQL_USER,
-  PASSWORD : process.env.MYSQL_ROOT_PASSWORD,
-  DATABASE : process.env.MYSQL_DATABASE,
+  HOST     : 'localhost',
+  USER    :   '<root>',
+  PASSWORD :  '<pass>',
+  DATABASE :  '<db>'
 }
