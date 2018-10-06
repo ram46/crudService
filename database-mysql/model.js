0e3476a5285d53d6dd0ea5444f29cfb0992ab7f3
key-er <nasaquba@gmail.com>
2018-10-05 22:52:30 -0700

    changed a bit

--- database-mysql/model.js
+++ database-mysql/model.js
@@ -5,15 +5,15 @@ const utils = require('./utils.js')
 
 module.exports = {
 
-  create: function(caseName, IOC, iocType, cb) {
-    utils.createIOC(caseName, IOC, iocType, (err, success) => {
-      if (err) cb('error in creating IOC', null)
+  create: function(query, cb) {
+    // query = {"caseName": "APT22" , "ioc": "file.exe", "ioctype": "file" }
+    ({caseName, ioc, ioctype} = JSON.parse(query))
+    utils.createIOC(caseName, ioc, ioctype, (err, success) => {
+      if (err) cb('error in creating ioc', null)
       if(success) {
         cb(null, 'create transaction succeeded')
       }
     })
-
-
     utils.searchInLogs(caseName, (err, done) => {
       if (err) cb('error in searching', null)
       if (done) cb(null, 'done searching')
@@ -21,7 +21,11 @@ module.exports = {
   },
 
 
-  read: function(caseName, versionNumber, cb) {
+  read: function(query, cb) {
+    // var query = {"caseName": "APT22", versionNum: 100}
+    // var query = {"caseName": "APT22"}
+    ({caseName, versionNumber} = JSON.parse(query))
+    versionNumber = parseInt(versionNumber) || 'latest'
     utils.readIOC(caseName, versionNumber, (err, result) => {
       if (err) cb('error in reading', null)
       if (result) cb(null, result)
@@ -29,53 +33,78 @@ module.exports = {
   },
 
 
-  update: function(caseName, fromValue, toValue, iocType, cb) {
-  // var old_query = {"newValue": {"ioc": "3.3.2.2"}, "where":  {"where": {"ioc" : "2.2.2.2"} }}
-    // new_query = {"caseName": "", "fromTo": {"", ""} }
+  update: function(query, cb) {
+    // query = {"caseName": "APT100", "fromValue": "8.8.8.8" :  toValue: "8.c" iocType: "file" }
+    ({caseName, fromValue, toValue, iocType} = JSON.parse(query))
     utils.updateIOC(caseName, fromValue, toValue, iocType, (err, success) => {
       if (err) cb('error in updating', null)
       if (success) cb(null , 'update transaction succeeded')
     })
   },
 
-  delete: function(caseName, iocToDelete, iocType, cb) {
+  delete: function(query, cb) {
+    // query = {"caseName": "APT100", iocToDelete": "8.c"  "iocType":"file"}
+    ({caseName, iocToDelete, iocType} = JSON.parse(query))
     utils.deleteIOC(caseName, iocToDelete, iocType, (err, success) => {
       if (err) cb('error in deleting', null)
       if (success) cb(null, 'delete transaction succeeded')
 
+    })
+  },
+
+  getAllCases: function(cb) {
+    utils.getAllCases((err, cases) => {
+      if (err) cb(err, null)
+      else cb(null, cases)
+    })
+  },
+
+
+  getCaseVersions: function(caseName, cb) {
+    utils.getCaseVersions(caseName, (err, caseVersions) => {
+      if (err) cb(err, null)
+      else cb(null, caseVersions)
     })
   }
 }
 
 
 
+var q1 = JSON.stringify({"caseName": "APT22" , "ioc": "44.exe", "ioctype": "file" })
+var q2 = JSON.stringify({"caseName": "APT22" , "ioc": "111.exe", "ioctype": "file" })
+var q3 = JSON.stringify({"caseName": "APT22" , "ioc": "6.6.6.6", "ioctype": "ip" })
+var q4 = JSON.stringify({"caseName": "APT22", "versionNumber": 100})
+var q5 = JSON.stringify({"caseName": "APT22", "fromValue": "6.6.6.6", "toValue": "66.c", "iocType": "file" })
+var q6 = JSON.stringify({"caseName": "APT22", "iocToDelete": "44.exe", "iocType":"file"})
 
-module.exports.create("APT120", "44.exe", "file", (err, result) => {
-    // module.exports.getDiffOfLastTwoVersions('APT120', (err, res) => {
-    //   console.log('apt120 case')
-    //   console.log('err is ', err);
-    //   console.log('res is ', res);
 
-  module.exports.create("APT100", "111.exe", "file", (err, result) => {
-  //   module.exports.createIOC("APT100", "7.7.7.7", "IP", (err, result) => {
+// module.exports.create(q1, (err, result) => {
+//     module.exports.getDiffOfLastTwoVersions('APT120', (err, res) => {
+//       console.log('apt120 case')
+//       console.log('err is ', err);
+//       console.log('res is ', res);
 
-  //     module.exports.createIOC("APT100", "111.exe", "file", (err, result) => {
-  //       module.exports.createIOC("APT100", "7.7.7.7", "IP", (err, result) => {
+// module.exports.create(q1, (err, result) => {
+//   module.exports.create(q2, (err, result) => {
+//     module.exports.create(q3, (err, result) => {
+//       module.exports.read(q4, (err, result) => {
+//       // module.exports.create("APT100", "111.exe", "file", (err, result) => {
+//       //   module.exports.create("APT100", "7.7.7.7", "IP", (err, result) => {
 
-  //         module.exports.updateIOC("APT100", "7.7.7.7", "5.5.5.5", "IP", (err, result) => {
-  //           module.exports.deleteIOC("33derder1.exe", "file", "APT100", (err, result) => {
+//           module.exports.update(q5, (err, result) => {
+//             module.exports.delete(q6, (err, result) => {
 
-  //             module.exports.deleteIOC("APT100", "5.5.5.5", "IP", (err, result) => {
+//               // module.exports.delete("APT100", "5.5.5.5", "IP", (err, result) => {
 
-  //             })
-  //           })
-  //         })
-  //       })
-  //     })
-  //   })
-  // })
-  })
-})
+//               })
+//             })
+//           // })
+//       //   })
+//       })
+//     })
+//   })
+// })
+// })
 
 
 // module.exports.create("APT120", "44.exe", "file", (err, result) => {
